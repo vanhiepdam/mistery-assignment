bfb6c21e915899777fbd498d86813717024521f0
Dam Hiep <hiep@kiuglobal.com>
2021-04-17 16:19:51 +0700

    add postgresql

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -6,3 +6,7 @@ from .base import *
 CORS_ALLOWED_ORIGINS = [
     'https://mistery-assignment.herokuapp.com'
 ]
+
+DATABASES = {
+    'default': dj_database_url.config(env='HEROKU_POSTGRESQL_GREEN_URL')
+}
